181c8f226b2d48aa49405f7d059ba8742620abdc
DannyBrito <britodanny1997@gmail.com>
2019-10-13 22:45:43 -0400

    Done.

--- tools/console.rb
+++ tools/console.rb
@@ -4,4 +4,18 @@ def reload
   load 'config/environment.rb'
 end
 
+carl = CarOwner.new("Carl")
+rick = CarOwner.new("Rick")
+mike = CarOwner.new("Mike")
+
+car1 = Car.new("Audi","R8","Sports")
+car2 = Car.new("Porsche","911","Sports")
+car3 = Car.new("Tesla", "Model X", "Electric")
+car4 = Car.new("Honda", "Accord", "Crossover")
+
+mec1 = Mechanic.new("Red", "Electric")
+mec2 = Mechanic.new("Blue", "Sports")
+mec3 = Mechanic.new("Green", "Crossover")
+mec4 = Mechanic.new("Brown", "Sports")
+
 binding.pry
